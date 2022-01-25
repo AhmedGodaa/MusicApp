a77ceb8da3ebba4fd61ae632cfc7998865ef8da7
ahmedgodaa <ana.a7med1140@gmail.com>
2022-01-26 01:11:30 +0200

    1 - Add permissions 2 - get Music from Storage

---
+++ app/src/main/java/com/examplez/musicapp/TapAccessorAdapter.java
@@ -0,0 +1,51 @@
+package com.examplez.musicapp;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+
+
+public class TapAccessorAdapter extends FragmentPagerAdapter {
+    public TapAccessorAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0 :
+                SongsFragment songsFragment = new SongsFragment();
+                return  songsFragment ;
+            case 1 :
+                AlbumFragment albumFragment = new AlbumFragment();
+                return  albumFragment ;
+
+            default: return  null ;
+
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position){
+            case 0 :
+                return "Songs" ;
+            case 1 :
+                return  "Albums" ;
+            default:
+                return null ;
+        }
+
+    }
+}
